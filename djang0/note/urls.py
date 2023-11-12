83598a78da70dd007289d197d2e3d088dd8298f2
Junmo1225 <junmo20091225@hotmail.com>
2023-11-12 13:54:32 +0900

    fix and notes connect

---
+++ djang0/note/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'note'
+
+urlpatterns = [
+    path('', views.note_list, name='note_list'),
+    # path('note/<int:note_id>/', views.note_detail, name='note_detail'),
+    # path('create_note/', views.create_note, name='create_note'),
+    # path('search_note/', views.search_note, name='search_note')
+]
--- djang0/note/views.py
+++ djang0/note/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from .models import Note
+# from.forms import NoteForm
+# from django.contrib.auth.decorators import login_required
+# from django.db.models import Q
 
-# Create your views here.
+# @login_required
+def note_list(request):
+    return render(request, 'note/note_list.html')
