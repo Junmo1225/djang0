d48d44d3979b822176f3f622976dd6eeff9f2e72
Junmo1225 <junmo20091225@hotmail.com>
2023-12-10 13:35:48 +0900

    12/3

--- djang0/note/urls.py
+++ djang0/note/urls.py
@@ -6,6 +6,6 @@ app_name = 'note'
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('<int:note_id>/', views.note_detail, name='note_detail'),
-    # path('create_note/', views.create_note, name='create_note'),
+    path('create_note/', views.create_note, name='create_note'),
     # path('search_note/', views.search_note, name='search_note')
 ]
--- djang0/note/views.py
+++ djang0/note/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Note
-# from.forms import NoteForm
+from .forms import NoteForm
 # from django.contrib.auth.decorators import login_required
 # from django.db.models import Q
 
@@ -12,3 +12,16 @@ def note_list(request):
 def note_detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'note/note_detail.html', {'note': note})
+
+def create_note(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST, request.FILES)
+        if form.is_valid():
+            note = form.save(commit=False)
+            # note.author = request.user
+            note.save()
+            return redirect('note:note_list')
+    else:
+        form = NoteForm()
+
+    return render(request, 'note/create_note.html', {'form': form})
